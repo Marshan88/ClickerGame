5dfabb80c01e8b98c5f18ea6254d0ddd0fc7665c
Marshan88 <mhenrix1@hotmail.com>
2022-02-25 11:13:54 +0100

    MVC
    CSS, MVC

---
+++ controller.js
@@ -0,0 +1,54 @@
+var points = 0;
+var pointsPerClick = 1000;
+var smileyIndex = 0;
+
+
+function doClick() {
+    points += pointsPerClick;
+    smileyIndex = getNextSmileyIndex(smileyIndex);
+    view();
+}
+
+function buyUpgrade() {
+    if (points < 10) return;
+       points -= 10;
+       pointsPerClick++;
+        view();
+}
+
+function omegaUpgrade() {
+    if (points >= 100){
+        points -= 100;
+        pointsPerClick *= 10;                
+    }
+        view();
+}
+
+function whatAjoke() {
+    if (points < 1000) return
+    points = 0;
+    pointsPerClick = 0;
+    updateView();
+    alert(`The programmer's wife sent him to the grocery store. Her instructions were: "buy milk. See if they have eggs. If they do, buy 10". So he went to the store and got back home with 10 packs of milk. When the wife asked why, he said: "they had eggs"`)
+}
+
+function reset() {
+    location.reload();
+}
+
+function getSmiley(aSmileyIndex) {
+    if(aSmileyIndex == 0) return '😀';
+    if(aSmileyIndex == 1) return '😁';
+    if(aSmileyIndex == 2) return '🤩';
+    if(aSmileyIndex == 3) return '🤠';
+    if(aSmileyIndex == 4) return '😈';
+}
+
+function getNextSmileyIndex(aSmileyIndex) {
+    const maxSmileyIndex = 4;
+    aSmileyIndex++;
+    if (aSmileyIndex > maxSmileyIndex) {
+        aSmileyIndex = 0;
+    }
+    return aSmileyIndex;
+}
---
+++ view.js
@@ -0,0 +1,14 @@
+view();
+function view() {
+    var smiley = getSmiley(smileyIndex);
+    document.getElementById('app').innerHTML = `
+        <h1>Clicker Game</h1>
+        <hr>
+        <div onclick="doClick()">${smiley}</div>
+        <div>${points}</div>
+        <button onclick="reset()">Reset</button><br>
+        <button onclick="buyUpgrade(10)">Upgrade (10 poeng)</button><br>
+        <button onclick="omegaUpgrade(100)">Omega upgrade (100 poeng)</button><br>
+        <button onclick="whatAjoke(10)">What a joke (1000 poeng)</button>
+    `;
+}
